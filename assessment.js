3f436198d36add8d1632fb4bb42618c1bbf042af
Jack Sweat <jacksweat@jacks-mbp.lan>
2020-03-27 18:13:08 -0700

    meddling with the code to no avail

--- assessment.js
+++ assessment.js
@@ -4,22 +4,28 @@
 let currentCount;
 //select the changing elements on html using js
 let newCount = document.getElementById("counter");
-let changedNumber = document.getElementById("input");
+let changedNumber = document.querySelector("num");
 
 //set click event listeners
-document.getElementById("add").onclick = addNumber;
-document.getElementById("sub").onclick = subtractNumber;
+document.getElementById("add").onclick = addNumber();
+document.getElementById("sub").onclick = subtractNumber();
 
 function init() { 
     changedNumber.value = 1;
-    newCount = 0;
+    currentCount = 0;
+}
+
+function render(){
+    newCount.innerHTML = currentCount;
 }
 function addNumber() {
-    currentCount = currentCount +  Math.floor(Math.changedNumber);
+    currentCount = currentCount +  Math.floor(changedNumber.value);
+    render();
 }
 
 function subtractNumber() {
-    currentCount = currentCount -  Math.floor(Math.changedNumber);
+    currentCount = currentCount -  Math.floor(changedNumber.value);
+    render();
 }
 
-init()
+init();
